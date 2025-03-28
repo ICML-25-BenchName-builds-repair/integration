d3dc841e57d6c72b115f14d3b0907bd6a39c8fe6
ubuntu <ubuntu@ip-10-0-0-64>
2025-03-28 12:58:29 +0000

    waleko__o1_17dc__id_60

--- tests/repositories/test_get_documentation.py
+++ tests/repositories/test_get_documentation.py
@@ -1,5 +1,5 @@
 
-from typing import Any
+from typing import Any, Dict
 import pytest
 from custom_components.hacs.base import HacsBase
 from custom_components.hacs.repositories.base import HacsRepository
@@ -13,7 +13,7 @@ from tests.common import client_session_proxy
     ({"installed": False, "last_version": "2.0.0"}, "Example readme file")
 ])
 @pytest.mark.asyncio
-async def test_validate_repository(hacs: HacsBase, data: dict[str, Any], result: str):
+async def test_validate_repository(hacs: HacsBase, data: Dict[str, Any], result: str):
     repository = HacsRepository(hacs=hacs)
     repository.data.full_name = "octocat/integration"
     for key, value in data.items():
